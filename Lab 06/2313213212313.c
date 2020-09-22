f04b5d88ebc3cb82106560a2c0f3ca81e9c5887a
hoangtri2k5 <71130618+hoangtri2k5@users.noreply.github.com>
2020-09-22 20:31:35 +0700

    lab 06

---
+++ Lab 06/2313213212313.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char *argv[]) 
+{
+	int num;
+	clrscr();
+	printf("The number from 1 to 30 are \n");
+	for (num = 2; num <= 30; num += 2);
+	scanf("%d\n", &num);
+	
+}
+
