a8d9d137e666ffce36b8f8124a2a886e4550b5d1
hoangtri2k5 <71130618+hoangtri2k5@users.noreply.github.com>
2020-12-07 21:14:01 +0700

    Create Lab04.sql

---
+++ Lab 04 sql/Lab04.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Students  
+(  
+StudentsID int,  
+FirstName varchar(255),  
+LastName varchar(255),  
+Email varchar(255),  
+AddressLine varchar(255),  
+City varchar(255)  
+)
+GO
+
+CREATE TABLE Class
+(
+ClassID int,
+TotalStudents int,
+)
+GO
